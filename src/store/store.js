cebd76c96e5f1487120e26d6828584390846ea04
sined27 <133235199+sined277@users.noreply.github.com>
2024-06-26 11:21:05 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import sneakersSlice from './slices/sneakersSlice'
 import cartSlice from './slices/cartSlice'
 
+
 export const store = configureStore({
     reducer: {
         sneakers: sneakersSlice,
